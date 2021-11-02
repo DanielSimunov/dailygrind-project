2cdf8156d69be94e6dcee17a17031d779fc7ece4
DanielSimunov <85276347+DanielSimunov@users.noreply.github.com>
2021-11-02 11:15:11 +1000

    Add files via upload

--- javascript/app.js
+++ javascript/app.js
@@ -4,8 +4,22 @@ var navbar = document.querySelector(".nav-container");
 document.addEventListener("scroll", function() {
     if (window.scrollY > 0) {
         navbar.classList.add("fade-in");
+        hamburgerMenu.classList.add("fade-in-menu");
     } else {
         navbar.classList.remove("fade-in");
+        hamburgerMenu.classList.remove("fade-in-menu");
     }
 });
 
+var hamburger = document.getElementById("hamburger");
+var hamburgerMenu = document.getElementById("hamburger-menu");
+
+hamburger.addEventListener('click', function() {
+    if (hamburgerMenu.style.height === '0px') {
+        hamburger.classList.add("open");
+        hamburgerMenu.style.height = '220px';
+    } else {
+        hamburger.classList.remove("open");
+        hamburgerMenu.style.height = '0px';
+    }
+});
--- javascript/index.js
+++ javascript/index.js
@@ -1,27 +1,13 @@
-// Selectors
 var slideIndex = [1, 1, 1];
 var slideId = ["slide1", "slide2", "slide3"]
 showSlides(1, 0);
 showSlides(1, 1);
 showSlides(1, 2);
 
-var navbar = document.querySelector(".nav-container");
-
-// Event Listeners
-document.addEventListener("scroll", function() {
-    if (window.scrollY > 0) {
-        navbar.classList.add("fade-in");
-    } else {
-        navbar.classList.remove("fade-in");
-    }
-});
-
-// Functions
 function plusSlides(n, no) {
     showSlides(slideIndex[no] += n, no);
 };
 
-// Slide function
 function showSlides(n, no) {
     var i;
     var x = document.getElementsByClassName(slideId[no]);
@@ -35,6 +21,31 @@ function showSlides(n, no) {
     // for (i = 0; i < dots.length; i++) {
     //     dots[i].className = dots[i].className.replace(" active", "");
     // }
-    x[slideIndex[no]-1].style.display = "block";
+    x[slideIndex[no]-1].style.display = "flex";
     // dots[slideIndex[no]-1].className += " active";
 };
+
+var hamburger = document.getElementById("hamburger");
+var hamburgerMenu = document.getElementById("hamburger-menu");
+
+hamburger.addEventListener('click', function() {
+    if (hamburgerMenu.style.height === '0px') {
+        hamburger.classList.add("open");
+        hamburgerMenu.style.height = '220px';
+    } else {
+        hamburger.classList.remove("open");
+        hamburgerMenu.style.height = '0px';
+    }
+});
+
+var navbar = document.querySelector(".nav-container");
+
+document.addEventListener("scroll", function() {
+    if (window.scrollY > 0) {
+        navbar.classList.add("fade-in");
+        hamburgerMenu.classList.add("fade-in-menu");
+    } else {
+        navbar.classList.remove("fade-in");
+        hamburgerMenu.classList.remove("fade-in-menu");
+    }
+});
