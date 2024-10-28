e3211784b96c8b70f9d29ed9b8abac815c2780af
DanielSimunov <85276347+DanielSimunov@users.noreply.github.com>
2024-10-28 18:59:24 +1000

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,13 @@
-var slideIndex = [1, 1, 1];
-var slideId = ["slide1", "slide2", "slide3"]
+var slideIndex = [1, 1, 1]; // Seperate index for each carousel
+var slideId = ["slide1", "slide2", "slide3"] // Class names for each carousel group
+var dotId = ["dot1", "dot2", "dot3"] // Class names for each set of dots for each carousel
+
+// Initial display of slides
 showSlides(1, 0);
 showSlides(1, 1);
 showSlides(1, 2);
 
+// Function to control next/prev buttons
 function plusSlides(n, no) {
     showSlides(slideIndex[no] += n, no);
 };
@@ -11,18 +15,19 @@ function plusSlides(n, no) {
 function showSlides(n, no) {
     var i;
     var x = document.getElementsByClassName(slideId[no]);
-    var dots = document.getElementsByClassName("dot");
+    var dots = document.getElementsByClassName(dotId[no]);
+    
     if (n > x.length) {slideIndex[no] = 1}
 
     if (n < 1) {slideIndex[no] = x.length}
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
-    // for (i = 0; i < dots.length; i++) {
-    //     dots[i].className = dots[i].className.replace(" active", "");
-    // }
+     for (i = 0; i < dots.length; i++) {
+         dots[i].className = dots[i].className.replace(" active", "");
+    }
     x[slideIndex[no]-1].style.display = "flex";
-    // dots[slideIndex[no]-1].className += " active";
+     dots[slideIndex[no]-1].className += " active";
 };
 
 var hamburger = document.getElementById("hamburger");
